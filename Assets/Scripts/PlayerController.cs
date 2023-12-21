470246388e50080982e939860e729641bbb2093e
DeividVA <david.verdugoabad@gmail.com>
2023-12-21 14:23:02 +0100

    jueves

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float jumpTime = 0.967f;
+    bool isGrounded = true;
+    Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+        _animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _animator.SetBool("IsRunning", Input.GetKey(KeyCode.UpArrow));
+        _animator.SetBool("IsJumping", Input.GetKeyDown(KeyCode.Space) && isGrounded);
+
+
+        if (_animator.GetBool("IsJumping")) 
+        { 
+            isGrounded = false;
+        }
+
+        if (!isGrounded)
+        {
+            jumpTime -= Time.deltaTime;
+        }
+
+        if (jumpTime <= 0) 
+        { 
+            isGrounded=true;
+            jumpTime = 0.967f;
+        }
+
+
+
+    }
+}
